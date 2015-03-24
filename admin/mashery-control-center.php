9e2c4b7a6858f4267af4caad4d3f06f907c91f89
Luis Gomez <lgomez@gmail.com>
2015-03-24 15:53:28 -0700

    Adding admin plugin

---
+++ admin/mashery-control-center.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Mashery Control Center
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: Mashery's Wordpress-powered Control Center
+ * Version: 0.0.1
+ * Author: Luis G. Gómez
+ * Author URI: http://luis.io
+ * License: Copyright 2015 Luis G. Gómez - All rights reserved.
+ */
+
+/**
+ * https://codex.wordpress.org/Creating_Admin_Themes
+ * https://codex.wordpress.org/Must_Use_Plugins
+ */

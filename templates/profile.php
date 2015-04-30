31d416ed1d43543029e93271a8d595d01b8ba748
Luis Gomez <lgomez@gmail.com>
2015-04-29 17:48:35 -0700

    Changing more variable names.

--- templates/profile.php
+++ templates/profile.php
@@ -1,22 +1,22 @@
 <table>
     <tr>
         <td>First Name</td>
-        <td><?= $user["first_name"] ?></td>
+        <td><?= $data["first_name"] ?></td>
     </tr>
     <tr>
         <td>Last Name</td>
-        <td><?= $user["last_name"] ?></td>
+        <td><?= $data["last_name"] ?></td>
     </tr>
     <tr>
         <td>E-Mail</td>
-        <td><?= $user["email"] ?></td>
+        <td><?= $data["email"] ?></td>
     </tr>
     <tr>
         <td>Twitter Handle</td>
-        <td><?= $user["twitter"] ?></td>
+        <td><?= $data["twitter"] ?></td>
     </tr>
     <tr>
         <td>Phone Number</td>
-        <td><?= $user["phone"] ?></td>
+        <td><?= $data["phone"] ?></td>
     </tr>
 </table>

596e6d5766904076841bd574fdc34f731a43aa56
Luis Gomez <lgomez@gmail.com>
2015-04-29 17:48:24 -0700

    Changing some variable names after refactoring

--- templates/documentation.php
+++ templates/documentation.php
@@ -1,7 +1,5 @@
 <p>These words and code are being dynamically generated. In the future dynamic documentation would show up here instead.</p>
-
-<p>API:<?= $api["key"]; ?></p>
-
+<p>API:<?= $data["key"]; ?></p>
 <code>
     // Code goes here.
 </code>
